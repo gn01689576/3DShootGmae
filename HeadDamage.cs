958b3a3b4f2ce5d4ff4be919c45b20f86c7ddb7a
gn01689576 <70142830+gn01689576@users.noreply.github.com>
2020-12-02 12:47:12 +0800

    寶相怪物頭部的傷害設定

---
+++ HeadDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadDamage : MonoBehaviour
+{
+    [SerializeField] float headdamage = 50;
+    hpCtrlREM hpCtrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamage damage = other.transform.root.GetComponent<IDamage>();
+        if (damage != null)
+        {
+            damage.Damage(headdamage);
+        }
+    }
+}
