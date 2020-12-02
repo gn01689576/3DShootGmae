17f13c78192d3fd110462a65da2a80f35de164c0
gn01689576 <70142830+gn01689576@users.noreply.github.com>
2020-12-02 12:50:02 +0800

    生怪的控制

---
+++ MonsterCave1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCave1: MonoBehaviour
+{
+    public GameObject monster;
+    public Transform REM;
+    
+
+    public float hardrate;
+    public float firerate;
+    float nextfire;
+    float ratee;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ratee = 1 + Time.time + hardrate;
+        if (Time.time > nextfire)
+        {
+            monster.GetComponent<monsterAICtrl1>().player = REM;
+            nextfire = Time.time + firerate;
+            GameObject monsterclone = Instantiate(monster, transform.position, transform.rotation) as GameObject;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject.Find("GameController").GetComponent<GameController>().dead = true;
+            return;
+        }
+    }
+}
