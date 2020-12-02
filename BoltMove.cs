4e4b721fe82e692534ad8bf3cc7ca12fe345cbd7
gn01689576 <70142830+gn01689576@users.noreply.github.com>
2020-12-02 12:42:48 +0800

    子彈的移動

---
+++ BoltMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BoltMove : MonoBehaviour
+{
+    public float speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        Destroy(gameObject, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
