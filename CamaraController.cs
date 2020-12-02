fa476fec00d0129334e1044814e8193101252bee
gn01689576 <70142830+gn01689576@users.noreply.github.com>
2020-12-02 12:44:23 +0800

    攝影機跟隨

---
+++ CamaraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+    
+
+public class CamaraController : MonoBehaviour
+{
+    public float speed;
+    public GameObject REM;
+    Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position - REM.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = REM.transform.position + offset;
+    }
+}
